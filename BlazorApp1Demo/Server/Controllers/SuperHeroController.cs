23cccb239ee2bde094e9e888f7bb769821beb065
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2023-11-28 10:08:15 -0300

    first commit

---
+++ BlazorApp1Demo/Server/Controllers/SuperHeroController.cs
@@ -0,0 +1,64 @@
+﻿using BlazorApp1Demo.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorApp1Demo.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly ILogger<SuperHeroController> _logger;
+
+        public static List<Comic> comics = new List<Comic> {
+            new Comic{ Id = 1, Name = "Marvel"},
+            new Comic{ Id = 2, Name = "DC"},
+            new Comic{ Id = 3, Name = "Wanley"}
+        };
+
+        public static List<SuperHero> heroes = new List<SuperHero> {
+            new SuperHero{
+                Id = 1,
+                FirstName = "Peter",
+                LastName = "Parker",
+                HeroName = "Miranha",
+                ComicId = 1,
+                Comic = comics[0]
+            },
+            new SuperHero{
+                Id = 2,
+                FirstName = "Bruce",
+                LastName = "Wayne",
+                HeroName = "Batman",
+                ComicId = 2,
+                Comic = comics[1]
+            },
+            new SuperHero{
+                Id = 3,
+                FirstName = "Tony",
+                LastName = "Stark",
+                HeroName = "Iron Man",
+                ComicId = 1,
+                Comic = comics[0]
+            }
+        };
+
+        public SuperHeroController(ILogger<SuperHeroController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes()
+        {
+            return Ok(heroes);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<SuperHero>> GetSuperHero(int id)
+        {
+            return Ok(heroes);
+        }
+
+    }
+}
